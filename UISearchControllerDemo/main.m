d5d92942e94b334ab555a182f973d49db977a113
李敏 <1512510162@qq.com>
2018-09-06 09:26:32 +0800

    Initial Commit

---
+++ UISearchControllerDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UISearchControllerDemo
+//
+//  Created by 李敏 on 2018/9/6.
+//  Copyright © 2018年 李敏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
